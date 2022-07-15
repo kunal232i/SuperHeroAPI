5e69f6bb90d12f8939a6f3d5ea21676466e0500f
kunal232i <kingawale23@gmail.com>
2022-07-15 07:55:25 +0000

    last change

--- script.js
+++ script.js
@@ -7,15 +7,38 @@ const heroImageDiv = document.getElementById("heroImage");
 const searchButton = document.getElementById("searchButton");
 const searchInput = document.getElementById("searchInput");
 
-let getSuperHero = (id, name) => {
+const getSuperHero = (id, name) => {
   fetch(`${BASE_URL}/${id}`)
     .then((response) => response.json())
     .then((json) => {
-      const name = `<h2>${json.name}</h2>`;
-      const intelligence = `<p>Intelligence: ${json.powerstats.intelligence}</p>`;
-      heroImageDiv.innerHTML = `${name}<img src="${json.image.url}" 
-      height=200 width=200/>${intelligence}`;
+      console.log(json.powerstats);
+      const superHero = json;
+      showHeroInfo(superHero);
     });
+};
+
+const statToEmoji = {
+  intelligence: "🧠",
+  strength: "💪",
+  speed: "⚡",
+  durability: "🏋️",
+  power: "📊",
+  combat: "⚔️",
+};
+
+const showHeroInfo = (character) => {
+  const name = `<h2>${character.name}</h2>`;
+
+  const img = `<img src="${character.image.url}" 
+  height=200 width=200/>`;
+
+  const stats = Object.keys(character.powerstats)
+    .map((stat) => {
+      return `<p>${statToEmoji[stat]} ${stat.toUpperCase()}: 
+    ${character.powerstats[stat]}</p>`;
+    })
+    .join("");
+  heroImageDiv.innerHTML = `${name}${img}${stats}`;
 };
 
 const randomHero = () => {
@@ -31,8 +54,7 @@ const getSearchSuperHero = (name) => {
     .then((response) => response.json())
     .then((json) => {
       const hero = json.results[0];
-      console.log(hero);
-      heroImageDiv.innerHTML = `<img src="${hero.image.url}"height=200 width=200/>`;
+      showHeroInfo(hero);
     });
 };
 
