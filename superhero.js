e0229bb1215ac5c9f2741d71b2ab2d2111fc02a2
kunal232i <kingawale23@gmail.com>
2023-08-13 16:52:36 +0530

    upgrade JS to TS

---
+++ superhero.js
@@ -0,0 +1,48 @@
+var SUPERHERO_TOKEN = "785737262585008";
+var BASE_URL = "https://superheroapi.com/api.php/".concat(SUPERHERO_TOKEN);
+var newHeroButton = document.getElementById("newHeroButton");
+var heroImageDiv = document.getElementById("heroImage");
+var searchButton = document.getElementById("searchButton");
+var searchInput = document.getElementById("searchInput");
+var getSuperHero = function (id) {
+    fetch("".concat(BASE_URL, "/").concat(id))
+        .then(function (response) { return response.json(); })
+        .then(function (json) {
+        console.log(json.powerstats);
+        var superHero = json;
+        showHeroInfo(superHero);
+    });
+};
+var statToEmoji = {
+    intelligence: "🧠",
+    strength: "💪",
+    speed: "⚡",
+    durability: "🏋️",
+    power: "📊",
+    combat: "⚔️",
+};
+var showHeroInfo = function (character) {
+    var name = "<h2>".concat(character.name, "</h2>");
+    var img = "<img src=\"".concat(character.image.url, "\" height=200 width=200/>");
+    var stats = Object.keys(character.powerstats)
+        .map(function (stat) {
+        return "<p>".concat(statToEmoji[stat], " ").concat(stat.toUpperCase(), ": ").concat(character.powerstats[stat], "</p>");
+    })
+        .join("");
+    heroImageDiv.innerHTML = "".concat(name).concat(img).concat(stats);
+};
+var randomHero = function () {
+    var numberOfHeroes = 731;
+    return Math.floor(Math.random() * numberOfHeroes) + 1;
+};
+newHeroButton.onclick = function () { return getSuperHero(randomHero()); };
+var getSearchSuperHero = function (name) {
+    console.log(searchInput.value);
+    fetch("".concat(BASE_URL, "/search/").concat(name))
+        .then(function (response) { return response.json(); })
+        .then(function (json) {
+        var hero = json.results[0];
+        showHeroInfo(hero);
+    });
+};
+searchButton.onclick = function () { return getSearchSuperHero(searchInput.value); };
---
+++ superhero.ts
@@ -0,0 +1,69 @@
+const SUPERHERO_TOKEN = "785737262585008";
+const BASE_URL: string = `https://superheroapi.com/api.php/${SUPERHERO_TOKEN}`;
+const newHeroButton = document.getElementById("newHeroButton") as HTMLButtonElement;
+const heroImageDiv = document.getElementById("heroImage") as HTMLElement;
+const searchButton = document.getElementById("searchButton") as HTMLButtonElement;
+const searchInput = document.getElementById("searchInput") as HTMLInputElement;
+
+interface PowerStats {
+  [key: string]: string;
+}
+
+interface SuperHero {
+  name: string;
+  image: {
+    url: string;
+  };
+  powerstats: PowerStats;
+}
+
+const getSuperHero = (id: number) => {
+  fetch(`${BASE_URL}/${id}`)
+    .then((response) => response.json())
+    .then((json: SuperHero) => {
+      console.log(json.powerstats);
+      const superHero = json;
+      showHeroInfo(superHero);
+    });
+};
+
+const statToEmoji: Record<string, string> = {
+  intelligence: "🧠",
+  strength: "💪",
+  speed: "⚡",
+  durability: "🏋️",
+  power: "📊",
+  combat: "⚔️",
+};
+
+const showHeroInfo = (character: SuperHero) => {
+  const name = `<h2>${character.name}</h2>`;
+  const img = `<img src="${character.image.url}" height=200 width=200/>`;
+
+  const stats = Object.keys(character.powerstats)
+    .map((stat) => {
+      return `<p>${statToEmoji[stat]} ${stat.toUpperCase()}: ${character.powerstats[stat]}</p>`;
+    })
+    .join("");
+
+  heroImageDiv.innerHTML = `${name}${img}${stats}`;
+};
+
+const randomHero = (): number => {
+  const numberOfHeroes = 731;
+  return Math.floor(Math.random() * numberOfHeroes) + 1;
+};
+
+newHeroButton.onclick = () => getSuperHero(randomHero());
+
+const getSearchSuperHero = (name: string) => {
+  console.log(searchInput.value);
+  fetch(`${BASE_URL}/search/${name}`)
+    .then((response) => response.json())
+    .then((json: { results: SuperHero[] }) => {
+      const hero = json.results[0];
+      showHeroInfo(hero);
+    });
+};
+
+searchButton.onclick = () => getSearchSuperHero(searchInput.value);
